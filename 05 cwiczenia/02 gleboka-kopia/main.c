8826d5632486ae90f95bc524f075a6f357c9fe65
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-01-28 21:44:51 +0100

    some updates

--- 05 cwiczenia/02 gleboka-kopia/main.c
+++ 05 cwiczenia/02 gleboka-kopia/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 struct tablica {
-	int tab[3];
+    int tab[3];
 };
 
-int main(){
+int main() {
 
-	struct tablica t1 = { {4, 2, 6} }, t2;
+    struct tablica t1 = {{4, 2, 6}}, t2;
 
-	t2 = t1;
-	t1.tab[2] = 30;
-	t2.tab[0] = 0;
+    t2 = t1; // głęboka kopia
+    t1.tab[2] = 30; //dla zilustrowania że zmienia się tylko t1
+    t2.tab[0] = 0;  //dla zilustrowania że zmienia się tylko t2
 
-	printf("t1 = {%d, %d, %d}\n", t1.tab[0], t1.tab[1], t1.tab[2]);
-	printf("t2 = {%d, %d, %d}\n", t2.tab[0], t2.tab[1], t2.tab[2]);
+    printf("t1 = {%d, %d, %d}\n", t1.tab[0], t1.tab[1], t1.tab[2]);
+    printf("t2 = {%d, %d, %d}\n", t2.tab[0], t2.tab[1], t2.tab[2]);
 
-	return 0;
+    return 0;
 }
