59464732fb53b72c316ce542cc2824b830bc2603
Grzegorz Borowik <grzesiu.borowik@gmail.com>
2018-02-08 14:42:20 +0100

    wykorzystanie push

--- 06 cwiczenia/03 stack-structure/main.c
+++ 06 cwiczenia/03 stack-structure/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "stos.h"
 
+int main(){
+    struct Wezel *top = NULL; //wskaznik stosu
+    struct Dane para;
+
+    para.indeks = 1;
+    para.wartosc = 23;
+    push(&top, para);
+
+    para.indeks = 2;
+    para.wartosc = 62;
+    push(&top, para);
+
+    para.indeks = 3;
+    para.wartosc = -35;
+    push(&top, para);
+
+    para.indeks = 4;
+    para.wartosc = 13;
+    push(&top, para);
+
+    return 0;
+}
