32962a92eb7df9d7d616c3b0b57d932173dc1289
Grzegorz Borowik <grzesiu.borowik@gmail.com>
2018-02-08 16:40:57 +0100

    woda 3 update

--- 06 cwiczenia/04 woda-w-histogramie 3/woda.c
+++ 06 cwiczenia/04 woda-w-histogramie 3/woda.c
@@ -1,5 +1,7 @@
 #include "woda.h"
 
+int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index);
+
 int slupWiekszyNizNaStosie(int popr, int nast, struct Wezel **pStos, int index){
     int tmpPopr = peek(*pStos).wartosc;
     int akumulator = (index - peek(*pStos).indeks)
@@ -19,15 +21,15 @@ int kiedyWzrasta(int popr, int nast, struct Wezel **pStos, int index) {
     } else if(peek(*pStos).wartosc <= nast){
         // sprawdzamy czy element na stosie
         // jest mniejszy od aktualnego Slupa
-        return slupWiekszyNizNaStosie(popr, nast, &pStos, index);
+        return slupWiekszyNizNaStosie(popr, nast, pStos, index);
     } else{
-        return slupMniejszyNizNaStosie(popr, nast, &pStos, index);
+        return slupMniejszyNizNaStosie(popr, nast, pStos, index);
     }
 }
 
 int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index){
     if(popr < nast) { //histogram wzrasta
-        kiedyWzrasta(popr, nast, &pStos, index);
+        kiedyWzrasta(popr, nast, pStos, index);
     } else if (popr > nast) { //histogram maleje
         struct Dane para = {index, popr};
         push(pStos, para);
