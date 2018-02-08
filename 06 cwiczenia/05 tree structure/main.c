69a8dfecdc9bf93c41cffb2c40e5b9ad257d1aa7
Grzegorz Borowik <grzesiu.borowik@gmail.com>
2018-02-08 18:39:13 +0100

    main

--- 06 cwiczenia/05 tree structure/main.c
+++ 06 cwiczenia/05 tree structure/main.c
@@ -3,7 +3,51 @@
 
 
 int main(){
+    struct Lisc *drzewo = NULL;
+    struct Dane dane;
+    int liczba = 0;
+    struct Wezel *stos = NULL;
+    FILE *file;
+    struct Dane *tab;
 
+    file = fopen("drzewo.bin", "wb+");
+    if(file == NULL){
+        printf("Blad otwarcia pliku!");
+        exit(EXIT_FAILURE);
+    }
+
+
+    do{
+        scanf("%d", &dane.wartosc);
+        dane.indeks = liczba;
+
+        add(dane, &drzewo);
+
+        ++liczba;
+    }while (dane.wartosc);
+
+    tab = (struct Dane*)malloc(liczba * sizeof(struct Dane));
+
+    zapiszDrzewo(file, drzewo);
+    rewind(file);
+    fread(tab, sizeof(struct Dane), liczba, file);
+
+    printf("Dane z tablicy:\n");
+    for (int i = 0; i < liczba; ++i) {
+        print_dane(tab[i]);
+    }
+    printf("\n");
+
+    printf("Drzewo:\n");
+    drukujDrzewo(1, drzewo);
+
+    flaten(&stos, drzewo);
+    printf("Stos:\n");
+    print(stos);
+
+    empty(&stos);
+    usunDrzewo(drzewo);
+    free(tab);
 
     return 0;
 }
