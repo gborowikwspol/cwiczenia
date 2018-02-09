e229bfafc833b0e11e3d143a96965ff33d1665aa
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-02-09 16:51:42 +0100

    woda update

--- 06 cwiczenia/01 woda-w-histogramie 1/main.c
+++ 06 cwiczenia/01 woda-w-histogramie 1/main.c
@@ -3,6 +3,8 @@
 
 int main() {
     int histogram[N] = {1, 2, 1, 1, 1, 1, 4, 2, 1, 1, 3, 1};
+//    int histogram[N] = {0, 1, 0, 2, 1, 0, 1, 5, 2, 1, 2, 1};
+//    int histogram[N] = {3, 5, 4, 2, 1, 7, 2};
 
     printf("Maksymalny stan zgromadzonej wody wynosi %d.\n", policzWode(histogram));
 
