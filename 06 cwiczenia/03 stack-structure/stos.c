2823221ad300912e25f7e9e7afc86932696215b7
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-02-09 16:17:09 +0100

    stos update

--- 06 cwiczenia/03 stack-structure/stos.c
+++ 06 cwiczenia/03 stack-structure/stos.c
@@ -1,31 +1,31 @@
 #include "stos.h"
 
-struct Wezel* nowy(struct Dane dane){
+// prywatna
+struct Wezel *nowy(struct Dane dane, struct Wezel *pTop) {
     struct Wezel *wezel;
 
-    wezel = (struct Wezel*)malloc(sizeof(struct Wezel));
-    if(wezel == NULL){
+    wezel = (struct Wezel *) malloc(sizeof(struct Wezel));
+    if (wezel == NULL) {
         printf("Brak pamieci!");
         exit(EXIT_FAILURE);
     }
 
     wezel->dane = dane;
-    wezel->popr = NULL;
+    wezel->popr = pTop;
 
     return wezel;
 }
 
-void push(struct Wezel **pTop, struct Dane dane){
-    struct Wezel* wezel = nowy(dane);
+void push(struct Wezel **pTop, struct Dane dane) {
+    struct Wezel *wezel = nowy(dane, *pTop); // w nowym wezle wskaz top
 
-    wezel->popr = *pTop; // w nowym wezle wskaz top
     *pTop = wezel; // top wskazuje teraz na nowy wezel
 
     printf("Para (%d, %d) odlozona na stosie\n",
            dane.indeks, dane.wartosc);
 }
 
-struct Dane pop(struct Wezel **pTop){
+struct Dane pop(struct Wezel **pTop) {
     struct Wezel *tymczasowy_wezel = *pTop;
     struct Dane odczytane_dane = tymczasowy_wezel->dane;
     *pTop = (*pTop)->popr;
@@ -34,30 +34,37 @@ struct Dane pop(struct Wezel **pTop){
     return odczytane_dane;
 }
 
-struct Dane peek(struct Wezel *pTop){
+struct Dane peek(struct Wezel *pTop) {
     return pTop->dane;
 }
 
-int isEmpty(struct Wezel *top){
+int isEmpty(struct Wezel *top) {
     return !top;
 }
 
-void print(struct Wezel *top){
-    if(!isEmpty(top)){
-        if(top->popr != NULL){
+void print(struct Wezel *top) {
+    if (!isEmpty(top)) {
+        if (top->popr != NULL) {
             print(top->popr);
         }
         print_dane(top->dane);
     }
 }
 
-void empty(struct Wezel **top){
-    while(!isEmpty(*top)){
-        pop(top);
+// prywatna
+void _empty(struct Wezel *pTop) {
+    if (pTop->popr != NULL) {
+        _empty(pTop->popr);
     }
+    free(pTop);
 }
 
-
+void empty(struct Wezel **pTop) {
+    if (!isEmpty(*pTop)) {
+        _empty(*pTop);
+        *pTop = NULL;
+    }
+}
 
 
 
