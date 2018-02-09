de1570766bf669f0da8681faba93ead88ab72405
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-02-09 14:20:55 +0100

    slupWiekszyNizNaStosie update

--- 06 cwiczenia/04 woda-w-histogramie 3/woda.c
+++ 06 cwiczenia/04 woda-w-histogramie 3/woda.c
@@ -2,50 +2,48 @@
 
 int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index);
 
-int slupWiekszyNizNaStosie(int popr, int nast, struct Wezel **pStos, int index){
-    int tmpPopr = peek(*pStos).wartosc;
-    int akumulator = (index - peek(*pStos).indeks)
-                     * (tmpPopr - popr);
-    pop(pStos);
+int slupWiekszyNizNaStosie(int popr, int nast, struct Wezel **pStos, int index) {
+    struct Dane dane = pop(pStos);
+    int akumulator = (index - dane.indeks) * (dane.wartosc - popr);
 
-    return akumulator + sprawdzPare(tmpPopr, nast, pStos, index);
+    return akumulator + sprawdzPare(dane.wartosc, nast, pStos, index); // rekurencyjnie
 }
 
-int slupMniejszyNizNaStosie(int popr, int nast, struct Wezel **pStos, int index){
+int slupMniejszyNizNaStosie(int popr, int nast, struct Wezel **pStos, int index) {
     return (index - peek(*pStos).indeks) * (nast - popr);
 }
 
 int kiedyWzrasta(int popr, int nast, struct Wezel **pStos, int index) {
-    if(isEmpty(*pStos)){
+    if (isEmpty(*pStos)) {
         return 0;
-    } else if(peek(*pStos).wartosc <= nast){
+    } else if (peek(*pStos).wartosc <= nast) {
         // sprawdzamy czy element na stosie
         // jest mniejszy od aktualnego Slupa
         return slupWiekszyNizNaStosie(popr, nast, pStos, index);
-    } else{
+    } else {
         return slupMniejszyNizNaStosie(popr, nast, pStos, index);
     }
 }
 
-int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index){
-    if(popr < nast) { //histogram wzrasta
+int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index) {
+    if (popr < nast) { //histogram wzrasta
         kiedyWzrasta(popr, nast, pStos, index);
     } else if (popr > nast) { //histogram maleje
         struct Dane para = {index, popr};
         push(pStos, para);
         return 0;
-    } else {
+    } else { //histogram stały
         return 0;
     }
 }
 
-int policzWode(int* histogram){
+int policzWode(int *histogram) {
     int woda = 0;
     struct Wezel *stos = NULL;
 
-    if(N < 3) return 0;
+    if (N < 3) return 0;
 
-    for(int index = 1; index < N; ++index){
+    for (int index = 1; index < N; ++index) {
         int popr = histogram[index - 1];
         int nast = histogram[index];
 
