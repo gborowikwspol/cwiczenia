39afd554e9f544bc76ae5befbc5252b125701a8b
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-02-09 14:21:13 +0100

    bug resolved

--- 06 cwiczenia/04 woda-w-histogramie 3/woda.c
+++ 06 cwiczenia/04 woda-w-histogramie 3/woda.c
@@ -27,7 +27,7 @@ int kiedyWzrasta(int popr, int nast, struct Wezel **pStos, int index) {
 
 int sprawdzPare(int popr, int nast, struct Wezel **pStos, int index) {
     if (popr < nast) { //histogram wzrasta
-        kiedyWzrasta(popr, nast, pStos, index);
+        return kiedyWzrasta(popr, nast, pStos, index);
     } else if (popr > nast) { //histogram maleje
         struct Dane para = {index, popr};
         push(pStos, para);
