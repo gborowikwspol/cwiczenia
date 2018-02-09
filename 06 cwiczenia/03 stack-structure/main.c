e1cbdadadee7a76aef01d02f00aa4f20eee8259f
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-02-09 16:18:54 +0100

    main update

--- 06 cwiczenia/03 stack-structure/main.c
+++ 06 cwiczenia/03 stack-structure/main.c
@@ -25,7 +25,7 @@ int main() {
 
     para_ze_stosu = pop(&top);
     printf("(%d, %d) zdjety ze stosu\n",
-             para_ze_stosu.indeks, para_ze_stosu.wartosc);
+           para_ze_stosu.indeks, para_ze_stosu.wartosc);
 
     printf("(%d, %d) najmlodszy na stosie\n",
            peek(top).indeks, peek(top).wartosc);
