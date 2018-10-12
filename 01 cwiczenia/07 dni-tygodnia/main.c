4e97a0cec70fc67a8d19e5266744f7cadecfaab8
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-10-12 09:50:27 +0200

    dni tygodnia

---
+++ 01 cwiczenia/07 dni-tygodnia/main.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include "stdio.h"
+
+int main() {
+    typedef enum {
+        ni, po = 5, wt, sr, cz, pi, so
+    } dni;
+
+    dni dzien_tygodnia;
+    bool czy_byl_obecny = false;
+
+    dzien_tygodnia = so;
+    printf("%i\n", dzien_tygodnia);
+
+    printf("%i\n", czy_byl_obecny);
+}
