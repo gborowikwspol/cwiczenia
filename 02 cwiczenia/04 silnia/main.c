0413ec3ab405404acdebf4d844da43deec5404fe
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-10-25 15:34:14 +0200

    silnia update

--- 02 cwiczenia/04 silnia/main.c
+++ 02 cwiczenia/04 silnia/main.c
@@ -1,25 +1,73 @@
 #include <stdio.h>
 
+#define TEST
+
+unsigned long licz_silnie(int podstawa) {
+    unsigned long licznik;
+    unsigned long silnia = 1;
+
+    for (licznik = 2; licznik <= podstawa; ++licznik) {
+        silnia *= licznik;
+    }
+    return silnia;
+}
+
+
+#ifdef TEST
+
+void nd(int licznik) { printf("Test nr %i nie działa\n", licznik); }
+
+void test_licz_silnie_0(int licznik) {
+    if (licz_silnie(0) != 1) nd(licznik);
+}
+
+void test_licz_silnie_1(int licznik) {
+    if (licz_silnie(1) != 1) nd(licznik);
+}
+
+void test_licz_silnie_2(int licznik) {
+    if (licz_silnie(2) != 2) nd(licznik);
+}
+
+void test_licz_silnie_3(int licznik) {
+    if (licz_silnie(5) != 120) nd(licznik);
+}
+
+void run_test_pool() {
+    int licznik = 0;
+
+    test_licz_silnie_0(++licznik);
+    test_licz_silnie_1(++licznik);
+    test_licz_silnie_2(++licznik);
+    test_licz_silnie_3(++licznik);
+}
+
+#endif
+
+
 int main() {
 
-    unsigned long int podstawa_silni;
-    unsigned long int licznik;
-    unsigned long int wynik = 1;
+#ifndef TEST
 
-    printf("Podaj wartosc calkowita dla ktorej mam obliczyc silnie, n = ");
-    scanf(" %ld", &podstawa_silni);
+    unsigned long wynik;
+    unsigned long podstawa;
 
-    if (podstawa_silni <= 15) {
+    printf("Podaj podstawe silni w przedziale od 0 do 15: \n");
+    scanf("%ld", &podstawa);
 
-        for (licznik = 2; licznik <= podstawa_silni; licznik++) {
-            wynik *= licznik;
-        }
-
-        printf("\nSilnia wynosi: %ld\n", wynik);
-
+    if (podstawa >= 0 && podstawa <= 15) {
+        wynik = licz_silnie(podstawa);
+        printf("silnia(%ld) = %ld", podstawa, wynik);
     } else {
-        printf("Za duza wartosc n, n <= 15");
+        printf("Podałeś liczbę spoza zakresu!\n");
     }
+
+#else
+
+    run_test_pool();
+
+#endif
 
     return 0;
 }
+
